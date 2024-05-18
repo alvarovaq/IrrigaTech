3950ce1753ad26e748e08f8626adad7f1cdc9142
Álvaro Vaquero Giménez <alvaro.vaquero.tel@gmail.com>
2024-05-18 16:56:16 +0200

    feat(api): Creamos documentacion y alguna configuracion de la API

--- irrigatech.api/src/main.ts
+++ irrigatech.api/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true
+  }));
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('IrrigaTech API Documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
