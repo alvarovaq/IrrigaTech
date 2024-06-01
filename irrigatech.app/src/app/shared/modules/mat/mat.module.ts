b95d9229977e23be6aa6972230420d4661eb909a
Álvaro Vaquero Giménez <alvaro.vaquero.tel@gmail.com>
2024-06-01 15:00:12 +0200

    feat(app): Creamos card para controlador manual

--- irrigatech.app/src/app/shared/modules/mat/mat.module.ts
+++ irrigatech.app/src/app/shared/modules/mat/mat.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button'; 
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';  
 
 const MatModules = [
   MatToolbarModule,
